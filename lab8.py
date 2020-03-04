8290b56d1591a9d542036f375234333e93d13b73
saudobak <smith_aurelia@columbusstate.edu>
2020-03-04 12:39:28 -0500

    lab8

--- lab8.py
+++ lab8.py
@@ -1,2 +1,41 @@
 #lab 8
-#boolen functions
+#boolean functions
+
+def isValidVoter(age):
+    #if statement - 18+
+    if age >= 18 and age <= 135:
+        return True
+    else:
+        return False
+
+def isLeapYear(year):
+    bFlag = False
+    if divisible by 4:
+
+        elif
+    else:
+       return bFlag 
+
+#define main()
+def main():
+    #ask user for name and age
+    name = 'Beth'
+    age = -5
+    year = 2020
+    
+    #call isValidVoter
+    if isValidVoter(age):
+        print('%s, at %d years old you are a valid voter' % (name, age))
+    else:
+        print('%s, at %d years old you are NOT a valid voter' % (name, age))
+
+    #call isLeapYear
+    if isLeapYear     ):
+        print('%s, at %d years old you are a valid voter' % (name, age))
+    else:
+        print('%s, at %d years old you are NOT a valid voter' % (name, age))    
+
+#call main()
+main()
+
+#nothing follows
